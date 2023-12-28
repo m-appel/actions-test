433d0ee48cb0c6482bebc0ad989d7e7507795867
Malte Tashiro <malte.appel@gmail.com>
2023-12-28 11:21:38 +0900

    broken file

---
+++ broken.py
@@ -0,0 +1,8 @@
+import bz2
+import argparse
+
+def main():
+    """This docstring is way too long it is over 88 characters and should be wrapped arroundnj"""
+    print("Hello")
+def no_space():
+    pass
