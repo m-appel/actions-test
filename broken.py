f080bc54d8dec832a60ad7f68faea14218e66c5a
Malte Tashiro <malte.appel@gmail.com>
2024-01-04 13:57:15 +0900

    unbreak + flake8

--- broken.py
+++ broken.py
@@ -1,10 +1,13 @@
-import bz2
-import argparse
 
 def main():
-    """This docstring is way too long it is over 88 characters and should be wrapped arroundnj"""
-    print("Hello")
+    """This docstring is way too long it is over 88 characters and should be wrapped
+    arround."""
+    print('Hello')
+
+
 def no_space():
     pass
+
+
 def even_less_space():
     return
